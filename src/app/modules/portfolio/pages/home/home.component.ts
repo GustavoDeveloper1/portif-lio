c5705512d253c994e1faa9c0473dfbfaaba09c31
GustavoDeveloper1 <devinvestidor02@gmail.com>
2023-12-28 09:02:20 -0300

    adicionando um heaeder

--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+
+import { HeaderComponentComponent } from '../../components/header-component/header-component.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponentComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
